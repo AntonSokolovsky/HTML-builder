37be79aae22e0b1aae95bd9a75400ca7f1412ef7
AntonSokolovsky <anton.a.sokolovsky@gmail.com>
2023-05-09 01:57:15 +0300

    feat: complete task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require("fs");
+const path = require("path");
 
+const pathFolder = path.join(__dirname);
+
+const pathFolderSrc = path.join(pathFolder, "files");
+const pathFolderDest = path.join(pathFolder, "files-copy");
+
+fs.mkdir(pathFolderDest, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+fs.readdir(pathFolderDest, { withFileTypes: true }, (error, data) => {
+  if(error) {
+    throw error;
+  } else {
+    data.forEach((el) => {
+      if(el.isFile()) {
+      const pathFolderFileDest = path.join(pathFolderDest, el.name);
+      fs.unlink(pathFolderFileDest, (err) => {
+        if(err) throw err;
+      });
+      }
+    })
+  }
+});
+fs.readdir(pathFolderSrc, { withFileTypes: true }, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    data.forEach((el) => {
+      if (el.isFile()) {
+        const pathFolderFileSrc = path.join(pathFolderSrc, el.name);
+        const pathFolderFileDest = path.join(pathFolderDest, el.name);
+        fs.copyFile(pathFolderFileSrc, pathFolderFileDest, (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+  }
+});
